d22f277d6dd62eaeadbbee03ba388b89a3eeba6f
Mateusz 'mBoiler' Marciniszyn <marciniszyn@pwapm.com>
2016-05-13 12:02:41 +0200

    set white background

--- core/src/qm/siusiak/colorline/ColorLine.java
+++ core/src/qm/siusiak/colorline/ColorLine.java
@@ -2,18 +2,25 @@ package qm.siusiak.colorline;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class ColorLine extends ApplicationAdapter {
 
+    SpriteBatch batch;
     private Line line;
     private LineGenerator lineGenerator;
     private float time;
-
+    private TextureRegion screenShot;
 
     @Override
     public void create() {
+        batch = new SpriteBatch();
         lineGenerator = new LineGenerator();
         line = lineGenerator.getLine();
+        screenShot = null;
     }
 
     @Override
@@ -21,12 +28,19 @@ public class ColorLine extends ApplicationAdapter {
 
         time += Gdx.graphics.getDeltaTime();
 
-        if (time > 0.5) {
+        if (time >0.2) {
             line = lineGenerator.getLine();
             time = 0;
         }
-
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if(screenShot != null) {
+            batch.begin();
+            batch.draw(screenShot, 0, 0);
+            batch.end();
+        }
         line.draw();
+        screenShot = ScreenUtils.getFrameBufferTexture();
     }
 
     @Override
@@ -34,4 +48,5 @@ public class ColorLine extends ApplicationAdapter {
         line.dispose();
         super.dispose();
     }
+
 }
